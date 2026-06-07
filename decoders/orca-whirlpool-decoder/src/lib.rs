73846838de5b29ac70d12209e967b9e36667c89f
oasisMystre <oasis.mystre@gmail.com>
2026-06-08 00:59:37 +0100

    feat: update raydium and orca programs

--- decoders/orca-whirlpool-decoder/src/lib.rs
+++ decoders/orca-whirlpool-decoder/src/lib.rs
@@ -1,4 +1,9 @@
-pub struct WhirlpoolDecoder;
+use solana_pubkey::Pubkey;
+
+pub struct OrcaWhirlpoolDecoder;
 pub mod accounts;
 pub mod instructions;
 pub mod types;
+
+pub const PROGRAM_ID: Pubkey =
+    Pubkey::from_str_const("whirLbMiicVdio4qvUfM5KAg6Ct8VwpYzGff3uctyCc");
